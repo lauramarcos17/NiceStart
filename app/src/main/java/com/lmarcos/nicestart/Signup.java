e2ed68666e3ee00e1cb02c3348ceeff5f9e94e87
Laura Marcos <lauramarcosmo@gmail.com>
2024-10-17 12:23:23 +0200

    añado cosas a las 12

--- app/src/main/java/com/lmarcos/nicestart/Signup.java
+++ app/src/main/java/com/lmarcos/nicestart/Signup.java
@@ -1,8 +1,6 @@
 package com.lmarcos.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    //el click del boton
-    public void openSignup(View v)
-    {
-        //CLASE QUE PERMITE ABRIR ACTIVITYS
-        Intent intent=new Intent(Login.this, Signup.class);
-        startActivity(intent);
-    }
-
-
 }
