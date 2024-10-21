8503e0b2fa3093d3ae8fc476b071cb0412f95cec
Laura Marcos <lauramarcosmo@gmail.com>
2024-10-21 13:17:04 +0200

    añado boton en registro

--- app/src/main/java/com/lmarcos/nicestart/Main.java
+++ app/src/main/java/com/lmarcos/nicestart/Main.java
@@ -1,8 +1,6 @@
 package com.lmarcos.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    //el click del boton
-    public void openSignup(View v)
-    {
-        //CLASE QUE PERMITE ABRIR ACTIVITYS
-        Intent intent=new Intent(Login.this, Signup.class);
-        startActivity(intent);
-    }
 
-
-    public void openMain(View view) {
-        Intent intent=new Intent(Login.this, Main.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/lmarcos/nicestart/Signup.java
+++ app/src/main/java/com/lmarcos/nicestart/Signup.java
@@ -1,6 +1,8 @@
 package com.lmarcos.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,17 @@ public class Signup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void openLogin()
+    {
+        Intent intent=new Intent(Signup.this, Login.class);
+        // PRIMERO SE PONE NOMBRE DE LA CLASE Y LUEGO LA QUE QUEREMOS ABRIR
+
+        startActivity(intent);
+    }
+
+    public void openMain(View view) {
+        Intent intento = new Intent(Signup.this, Main.class);
+        startActivity(intento);
     }
 }
